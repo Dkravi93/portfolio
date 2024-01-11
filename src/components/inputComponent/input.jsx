7c09185efcbfc131d9125111bd9f4f789486e9e5
Deepak Kumar Ravi <deepak.kumar@appfoster.com>
2024-01-11 15:15:33 +0530

    Resume the email service

--- src/components/inputComponent/input.jsx
+++ src/components/inputComponent/input.jsx
@@ -1,39 +1,84 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const ContactUs = () => {
-  const form = useRef();
+  const [formData, setFormData] = useState({
+    from_name: "",
+    from_email: "",
+    message: "",
+  });
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_381fbjb', 'template_v6qpepg', form.current, 'QLDskRbVad7C7lLQx')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-      e.target.reset();
+    if (!formData.from_name || !formData.from_email) {
+      toast.error("Please add all required fields!");
+    } else {
+      emailjs
+        .sendForm(
+          process.env.REACT_APP_SERVICE_ID,
+          process.env.REACT_APP_TEMPLATE_ID,
+          e.target,
+          process.env.REACT_APP_PUBLIC_KEY
+        )
+        .then(
+          (result) => {
+            toast.success("Wow, You Are Awesome! 😊😊😊");
+            console.log(result.text);
+          },
+          (error) => {
+            toast.error("Error, Working on it!");
+            console.log(error.text);
+          }
+        );
+      setFormData({ from_name: "", from_email: "", message: "" });
+    }
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <br/>
-      <input type="text" name="from_name" />
-      <br/>
-      <br/>
-      <label>Email</label>
-      <br/>
-      <input type="email" name="from_email" />
-      <br/>
-      <br/>
-      <label>Message</label>
-      <br/>
-      <textarea id="message" name="message" />
-      <br/>
-      <br/>
-      <input type="submit" value="Send" />
-    </form>
+    <>
+      <form onSubmit={sendEmail}>
+        <label>Name</label>
+        <br />
+        <input
+          type="text"
+          name="from_name"
+          value={formData.from_name}
+          onChange={(e) =>
+            setFormData({ ...formData, from_name: e.target.value })
+          }
+        />
+        <br />
+        <br />
+        <label>Email</label>
+        <br />
+        <input
+          type="email"
+          name="from_email"
+          value={formData.from_email}
+          onChange={(e) =>
+            setFormData({ ...formData, from_email: e.target.value })
+          }
+        />
+        <br />
+        <br />
+        <label>Message</label>
+        <br />
+        <textarea
+          id="message"
+          name="message"
+          value={formData.message}
+          onChange={(e) =>
+            setFormData({ ...formData, message: e.target.value })
+          }
+        />
+        <br />
+        <br />
+        <input type="submit" value="Send" />
+      </form>
+      <ToastContainer />
+    </>
   );
 };
